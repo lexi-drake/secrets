cb9c3ce146c781e2b3251637e80a66cfe1bc8c72
Alexa Drake <alexa11drake@gmail.com>
2018-10-04 11:11:43 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Secrets #
+
+A tool for handling secrets in go.
---
+++ src/secrets.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"reflect"
+)
+
+type Secrets interface {
+	Validate() bool
+}
+
+func LoadFromJson(filename string, secrets Secrets) error {
+	secrets_file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(secrets_file, &secrets)
+	return err
+}
+
+func LoadFromEnvironment(secrets Secrets) {
+	v := reflect.ValueOf(secrets).Elem()
+	t := v.Type()
+	
+	for index := 0; index < v.NumField(); index++ {
+		field := t.Field(index)
+		json_value := field.Tag.Get("json")
+		value := os.Getenv(json_value)
+		if value != "" {
+			log.Println(json_value + " has value " + value)
+			v.Field(index).SetString(value)
+		}
+	}
+}
